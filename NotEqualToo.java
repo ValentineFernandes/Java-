c7a53748390327db22d157a56e5858b5b5b6d34b
Valentine <valentineinfantinejohn@gmail.com>
2022-02-16 01:01:02 +0530

    Create NotEqualToo.java

---
+++ NotEqualToo.java
@@ -0,0 +1,16 @@
+public class NotEqualToo
+{
+  public static void main(String[] args)
+  {
+	  int x=10;
+	  int y=10;
+	  if(x!=y)
+	  {
+		  System.out.println("The Values are different");
+	  }
+    else
+    {
+		  System.out.println("The Values are same");
+    }
+  }
+}
