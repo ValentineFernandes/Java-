c10012ca2c0f33b26d75db366bbdd62879d8f3bf
Valentine <valentineinfantinejohn@gmail.com>
2022-02-16 01:21:58 +0530

    Create Uppercase.java

---
+++ Uppercase.java
@@ -0,0 +1,8 @@
+public class UpperCase
+{
+  public static void main(String[] args)
+  {
+	  String x="a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
+	  System.out.println(x.toUpperCase());
+  }
+}
