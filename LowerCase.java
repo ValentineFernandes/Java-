398424bdcafa3d349ffa752c57a065244c38d0e2
Valentine <valentineinfantinejohn@gmail.com>
2022-02-16 00:47:33 +0530

    Create LowerCase.java

---
+++ LowerCase.java
@@ -0,0 +1,8 @@
+public class LowerCase
+{
+  public static void main(String[] args)
+  {
+	  String x="A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
+	  System.out.println(x.toLowerCase());
+  }
+}
