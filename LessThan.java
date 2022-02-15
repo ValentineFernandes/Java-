a2902a3b621b300d1f5a4059593534fb676d8b27
Valentine <valentineinfantinejohn@gmail.com>
2022-02-16 00:43:08 +0530

    Create LessThan.java

---
+++ LessThan.java
@@ -0,0 +1,16 @@
+public class LessThan
+{
+  public static void main(String[] args)
+  {
+	  int x=50;
+	  int y=40;
+	  if(x<y)
+	  {
+		  System.out.println("x is smaller than y");
+	  }
+    else
+    {
+		  System.out.println("y is small than x");
+    }
+  }
+}
