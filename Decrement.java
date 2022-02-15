bf6ac1db3cd9032859c43cb2238c4ee03b290d7f
Valentine <valentineinfantinejohn@gmail.com>
2022-02-16 00:19:56 +0530

    Create Decrement.java

---
+++ Decrement.java
@@ -0,0 +1,21 @@
+public class Decrement
+{ 
+  public static void main(String[] args)	
+	{
+	  int a=5,b=5;
+	  System.out.println("The value of a is: "+a--);
+	  System.out.println("The value of a is: "+a--);
+	  System.out.println("The value of a is: "+a--);
+	  System.out.println("The value of a is: "+a--);
+	  System.out.println("The value of a is: "+a--);
+	  System.out.println("The value of a is: "+a--);
+	  System.out.println("---------------------------------");
+	  System.out.println("The value of b is :"+--b);
+	  System.out.println("The value of b is :"+--b);
+	  System.out.println("The value of b is :"+--b);
+	  System.out.println("The value of b is :"+--b);
+	  System.out.println("The value of b is :"+--b);
+	  System.out.println("The value of b is :"+--b);
+  }
+}
+
