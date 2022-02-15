c084f18e3711633226f14ca909727e5a6a4cc30e
Valentine <valentineinfantinejohn@gmail.com>
2022-02-16 00:30:20 +0530

    Create Increment.java

---
+++ Increment.java
@@ -0,0 +1,13 @@
+public class Increment
+{  
+	public static void main(String[] args)	
+	{
+	  int a=5;
+	  System.out.println("The value of a is: "+a++);
+	  System.out.println("The value of a is: "+a++);
+	  System.out.println("The value of a is: "+a++);
+	  System.out.println("The value of a is: "+a++);
+	  System.out.println("The value of a is: "+a++);
+	  System.out.println("The value of a is: "+a++);
+  }
+}
