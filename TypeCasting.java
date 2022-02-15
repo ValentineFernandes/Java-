ac329e46cc5c8313152617864806b4607b110bbe
Valentine <valentineinfantinejohn@gmail.com>
2022-02-16 01:20:48 +0530

    Create TypeCasting.java

---
+++ TypeCasting.java
@@ -0,0 +1,10 @@
+public class TypeCasting
+{
+	public static void main(String[] args)
+	{		
+		float b=9.25f;
+		int c=b;
+		System.out.println("The value of b is: "+b);
+		System.out.println("The value of c is: "+c);
+	}
+}
