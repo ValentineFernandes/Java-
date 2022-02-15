a6cf547e8578d90fa234c182657840d4e90924f1
Valentine <valentineinfantinejohn@gmail.com>
2022-02-16 00:58:01 +0530

    Create NarrowCasting.java

---
+++ NarrowCasting.java
@@ -0,0 +1,10 @@
+public class NarrowCasting
+{
+	public static void main(String[] args)
+	{	
+		float b=9.25f;
+		int c=(int)b;
+		System.out.println("The value of b is: "+b);
+		System.out.println("The value of c is: "+c);
+	}
+}
