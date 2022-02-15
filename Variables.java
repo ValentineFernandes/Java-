0b5f3cf915916ede83e4866703f16c61c487cc2b
Valentine <valentineinfantinejohn@gmail.com>
2022-02-16 01:23:47 +0530

    Create Variables.java

---
+++ Variables.java
@@ -0,0 +1,18 @@
+public class Variables
+{   
+  public static void main(String[] args)
+	{
+	  int a=5;            
+	  float b=9.12f;    
+	  double c=9.152; 
+	  char d='A' ;       
+	  String e="Lecture"; 
+	  boolean f=true;        
+	  System.out.println("The value of a is: "+a);
+	  System.out.println("The value of b is: "+b);
+	  System.out.println("The value of c is: "+c);
+	  System.out.println("The value of d is: "+d);
+	  System.out.println("The value of e is: "+e);
+	  System.out.println("The value of f is: "+f);
+	}
+}
