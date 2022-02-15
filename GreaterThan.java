37e19f7dafc6e39579a91b9fa12b280e3451a1fd
Valentine <valentineinfantinejohn@gmail.com>
2022-02-16 00:28:51 +0530

    Create GreaterThan.java

---
+++ GreaterThan.java
@@ -0,0 +1,16 @@
+public class GreaterThan
+{
+  public static void main(String[] args)
+  {
+	  int x=20;
+	  int y=40;
+	  if(x>y)
+	  {
+		  System.out.println("The x is bigger than y");
+	  }
+    else
+    {
+		  System.out.println("The y is bigger than x");
+    }
+  }
+}
