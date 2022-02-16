b05014b1dd7263474b25b55df489a898106db871
Valentine <valentineinfantinejohn@gmail.com>
2022-02-16 09:58:38 +0530

    Create MultipleIfOr.java

---
+++ MultipleIfOr.java
@@ -0,0 +1,15 @@
+public class MultipleIfOr
+{
+  public static void main(String[] args)
+  {
+    int hindi=25, english=24, marathi=26,maths=26, science=28, ss=99;
+    if(hindi>=35||english>=35||marathi>=35||maths>=35||science>=35||ss>=35)
+    {
+      System.out.println("You are pass");
+    }
+    else
+    {
+      System.out.println("You are fail");
+    }
+  }
+}
