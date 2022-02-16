454cbf7d7d6a84ba0499f20c59f7736f4c5b4fe3
Valentine <valentineinfantinejohn@gmail.com>
2022-02-16 09:13:30 +0530

    Create Package1.java

---
+++ Package1.java
@@ -0,0 +1,12 @@
+import java.util.*; 
+class Package1 
+{
+  public static void main(String[] args) 
+  {
+    Scanner myObj = new Scanner(System.in);
+    String userName;
+    System.out.println("Enter username"); 
+    userName = myObj.nextLine();   
+    System.out.println("Username is: " + userName);        
+  }
+}
