f3602223dac5da3760fd14f0ae3498dad3435537
Valentine <valentineinfantinejohn@gmail.com>
2022-02-16 10:15:24 +0530

    Create NestedIf.java

---
+++ NestedIf.java
@@ -0,0 +1,18 @@
+public class NestedIf 
+{    
+  public static void main(String[] args) 
+  {    
+    int i=10;
+    if(i==10) 
+    {    
+      if(i<15) 
+      {    
+        System.out.println("i is smaller than 15");    
+      }
+      else 
+      {    
+        System.out.println("i is greater than 15");    
+      }    
+    }    
+  }   
+} 
