e4cdeafca952a4dd1c3b118a43cf4a706c1f5e22
Valentine <valentineinfantinejohn@gmail.com>
2022-02-16 09:42:18 +0530

    Create IfGreaterThanEqualTo.java

---
+++ IfGreaterThanEqualTo.java
@@ -0,0 +1,12 @@
+public class IfGreaterThanEqualTo 
+{
+  public static void main(String[] args) 
+  {
+    int x = 10;
+    int y = 8;
+    if (x >= y) 
+    {
+      System.out.println("x is greater than or equal to y");
+    }  
+  }
+}
