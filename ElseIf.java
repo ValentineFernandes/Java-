a61a9bfa780e579ad1f83c251a24cb8a2006d885
Valentine <valentineinfantinejohn@gmail.com>
2022-02-16 10:00:57 +0530

    Create ElseIf.java

---
+++ ElseIf.java
@@ -0,0 +1,20 @@
+public class ElseIf 
+{
+  public static void main(String[] args)
+  {
+    int a = 35;
+    int b = 35;
+    if (a > b) 
+    {
+      System.out.println("a is greater than b");
+    }
+    else if(a<b)
+    {
+      System.out.println("a is smaller than b");
+    }
+    else
+    {
+      System.out.println("both values are equal");
+    }
+  }
+}
