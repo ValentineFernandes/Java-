aa9352199e3c9586a772be782750f54fee1d52fb
Valentine <valentineinfantinejohn@gmail.com>
2022-02-16 09:41:30 +0530

    Create IfGreaterThan.java

---
+++ IfGreaterThan.java
@@ -0,0 +1,12 @@
+public class IfGreaterThan 
+{
+  public static void main(String[] args) 
+  {
+    int x = 10;
+    int y = 8;
+    if (x > y) 
+    {
+      System.out.println("x is greater than y");
+    }  
+  }
+}
