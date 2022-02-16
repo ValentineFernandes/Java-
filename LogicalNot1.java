d52363ab99e3142e25aef01bb94b58e81fd13aea
Valentine <valentineinfantinejohn@gmail.com>
2022-02-16 09:36:16 +0530

    Create LogicalNot1.java

---
+++ LogicalNot1.java
@@ -0,0 +1,15 @@
+public class LogicalNot1
+{
+  public static void main(String[] args)
+  {
+	  int a=36,b=45,c=33,d=37,e=35;
+	  if(!(a>=35&&b>=35&&c>=35&&d>=35&&e>=35))
+	  { 	
+		  System.out.println("You're Pass");
+    }
+    else
+    {
+	    System.out.println("You're Fail");
+    }
+  }
+}
