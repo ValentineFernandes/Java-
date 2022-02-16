7bbf0f17abc57caa82194e9688dbec22bdb50c48
Valentine <valentineinfantinejohn@gmail.com>
2022-02-16 09:32:19 +0530

    Create LessThanEqualTo2.java

---
+++ LessThanEqualTo2.java
@@ -0,0 +1,16 @@
+public class LessThanEqualTo2
+{
+  public static void main(String[] args)
+  {
+	  int x=40;
+	    int y=20;
+	  if(x<=y)
+	  {
+		  System.out.println("x is less than or equal to y");
+	  }
+    else
+    {
+		  System.out.println("y is less than or equal to x");
+    }
+  }
+}
