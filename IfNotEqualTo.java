ab26877fcdc33ae4b7baf39ff94b32f3a2bb152e
Valentine <valentineinfantinejohn@gmail.com>
2022-02-16 09:43:56 +0530

    Create IfNotEqualTo.java

---
+++ IfNotEqualTo.java
@@ -0,0 +1,12 @@
+public class IfNotEqualTo 
+{
+  public static void main(String[] args) 
+  {
+    int x = 10;
+    int y = 11;
+    if (x != y) 
+    {
+      System.out.println("x is not equal to y");
+    }  
+  }
+}
