27624ab5dcf1580653a49c44a0f048ca9f6dcb29
Valentine <valentineinfantinejohn@gmail.com>
2022-02-16 10:13:49 +0530

    Create IfStatement.java

---
+++ IfStatement.java
@@ -0,0 +1,12 @@
+public class ifstatement 
+{    
+  public static void main(String[] args) 
+  {    
+    int x = 10;    
+    int y = 12;    
+    if(x+y > 20) 
+    {    
+      System.out.println("x + y is greater than 20");    
+    }    
+  }      
+}    
