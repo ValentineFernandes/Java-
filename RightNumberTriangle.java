01484158ee85a50595eb8c0383616be6720e6218
Valentine <valentineinfantinejohn@gmail.com>
2022-02-16 09:48:20 +0530

    Create RightNumberTriangle.java

---
+++ RightNumberTriangle.java
@@ -0,0 +1,17 @@
+public class RightNumberTriangle  
+{  
+  public static void main(String args[])   
+  {   
+    int i, j,num, n=5;   
+    for(i=0; i<n; i++)  
+    {   
+      num=1;   
+      for(j=0; j<=i; j++)  
+      {   
+        System.out.print(num+ " ");     
+        num++;   
+      }   
+      System.out.println();   
+    }   
+  }   
+}  
