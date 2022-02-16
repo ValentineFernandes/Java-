958f74263b7582222b86620c731427c9a0f8890e
Valentine <valentineinfantinejohn@gmail.com>
2022-02-16 09:39:53 +0530

    Create IfLessThan.java

---
+++ IfLessThan.java
@@ -0,0 +1,12 @@
+public class IfLessThan 
+{
+  public static void main(String[] args) 
+  {
+    int x = 10;
+    int y = 18;
+    if (x < y) 
+    {
+      System.out.println("x is less than y");
+    }  
+  }
+}
