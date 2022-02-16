c99db8b6fe6b0f297f80d1a7bd0bc9b71179a67b
Valentine <valentineinfantinejohn@gmail.com>
2022-02-16 08:59:40 +0530

    Create DateandTime.java

---
+++ DateandTime.java
@@ -0,0 +1,9 @@
+import java.time.LocalDateTime; 
+public class DateandTime 
+{
+  public static void main(String[] args) 
+  {
+    LocalDateTime myObj = LocalDateTime.now();
+    System.out.println(myObj);
+  }
+}
