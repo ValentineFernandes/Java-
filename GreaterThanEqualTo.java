07c0b5bc546efb117ca8d437e0521692cc779de3
Valentine <valentineinfantinejohn@gmail.com>
2022-02-16 09:20:55 +0530

    Create GreaterThanEqualTo.java

---
+++ GreaterThanEqualTo.java
@@ -0,0 +1,16 @@
+public class GreaterThanEqualTo
+{
+  public static void main(String[] args)
+  {
+	  int x=20;
+	  int y=20;
+	  if(x>=y)
+	  {
+		  System.out.println("x is greater than or equal to y");
+	  }
+    else
+    {
+      System.out.println("y is greater than or equal to x");
+    }
+  }
+}
