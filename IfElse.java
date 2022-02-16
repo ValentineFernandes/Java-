f28f8f482e855489e2dcd5e5321e15893cad0ff5
Valentine <valentineinfantinejohn@gmail.com>
2022-02-16 10:01:46 +0530

    Create IfElse.java

---
+++ IfElse.java
@@ -0,0 +1,16 @@
+public class IfElse 
+{
+  public static void main(String[] args) 
+  {
+    int a = 35;
+    int b = 35;
+    if (a > b) 
+    {
+      System.out.println("a is greater than b");
+    }
+    else
+    {
+      System.out.println("b is greater than a");
+    }
+  }
+}
