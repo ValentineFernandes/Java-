1e262afb0ecb5271b5efe6ae46526b3bb9e634ed
Valentine <valentineinfantinejohn@gmail.com>
2022-02-16 09:43:01 +0530

    Create IfEqualTo.java

---
+++ IfEqualTo.java
@@ -0,0 +1,12 @@
+public class IfEqualTo 
+{
+  public static void main(String[] args) 
+  {
+    int x = 10;
+    int y = 10;
+    if (x == y) 
+    {
+      System.out.println("x is equal to y");
+    }  
+  }
+}
