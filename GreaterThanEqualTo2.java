c849bb3448a5011da26ee0e003de77d934b0ba02
Valentine <valentineinfantinejohn@gmail.com>
2022-02-16 09:25:28 +0530

    Update GreaterThanEqualTo2.java

--- GreaterThanEqualTo2.java
+++ GreaterThanEqualTo2.java
@@ -1,17 +1,16 @@
-public class GreaterThanEqualTo2{
-public static void main(String[] args)
+public class GreaterThanEqualTo2
 {
+public static void main(String[] args)
+    {
 	int x=10;
 	int y=20;
 	if(x>=y)
 	{
 		System.out.println("x is greater than or equal to y");
 	}
-else
-{
-
+	else
+	{
 		System.out.println("y is greater than or equal to x");
-}
-}
-
+	}
+    }
 }
