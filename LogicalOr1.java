03619225e27b97eb7117d377b946d5e0e054b83e
Valentine <valentineinfantinejohn@gmail.com>
2022-02-16 09:35:05 +0530

    Create LogicalOr1.java

---
+++ LogicalOr1.java
@@ -0,0 +1,15 @@
+public class LogicalOr1
+{
+  public static void main(String[] args)
+  {
+	  int a=40,b=45,c=37,d=35,e=34;
+	  if(a>=35||b>=35||c>=35||d>=35||e>=35)
+	  { 	
+		  System.out.println("You're Pass");
+    }
+    else
+    {
+	    System.out.println("You're Fail");
+    }
+  }
+}
