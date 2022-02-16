753b2c33649c9179931332da174218e33f7324c5
Valentine <valentineinfantinejohn@gmail.com>
2022-02-16 09:31:18 +0530

    Create LessThanEqualTo1.java

---
+++ LessThanEqualTo1.java
@@ -0,0 +1,16 @@
+public class LessThanEqualTo1
+{
+  public static void main(String[] args)
+  {
+	  int x=20;
+	  int y=20;
+	  if(x<=y)
+	  {
+		  System.out.println("x is less than or equal to y");
+	  }
+    else
+    {
+		  System.out.println("y is less than or equal to x");
+    }
+  }
+}
