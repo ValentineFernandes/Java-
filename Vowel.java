00068c874df88b971e6ca45516b1ed1d81c20d07
Valentine <valentineinfantinejohn@gmail.com>
2022-02-16 10:04:15 +0530

    Create Vowel.java

---
+++ Vowel.java
@@ -0,0 +1,24 @@
+public class Vowel {
+    public static void main(String[] args) {
+        char ab = 'a';
+        switch (ab) {
+            case 'a':
+                  System.out.println(ab + " is vowel");
+                  break;
+            case 'e':
+                  System.out.println(ab + " is vowel");
+                  break;
+            case 'i':
+                  System.out.println(ab + " is vowel");
+                  break;
+            case 'o':
+                  System.out.println(ab + " is vowel");
+                  break;
+            case 'u':
+                  System.out.println(ab + " is vowel");
+                  break;
+            default:
+                  System.out.println(ab + " is consonant");
+        }
+    }
+}
