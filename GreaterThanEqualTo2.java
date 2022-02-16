83b06266e0740f5583ad62d0cca7e6f13a20d1d6
Valentine <valentineinfantinejohn@gmail.com>
2022-02-16 09:24:27 +0530

    Add files via upload

---
+++ GreaterThanEqualTo2.java
@@ -0,0 +1,17 @@
+public class GreaterThanEqualTo2{
+public static void main(String[] args)
+{
+	int x=10;
+	int y=20;
+	if(x>=y)
+	{
+		System.out.println("x is greater than or equal to y");
+	}
+else
+{
+
+		System.out.println("y is greater than or equal to x");
+}
+}
+
+}
