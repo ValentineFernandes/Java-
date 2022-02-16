05043c698438a681cc6d1aca45f690e98431f619
Valentine <valentineinfantinejohn@gmail.com>
2022-02-16 10:10:51 +0530

    Create PostandPre.java

---
+++ PostandPre.java
@@ -0,0 +1,44 @@
+public class PostandPre
+{
+  public static void main(String[] args)
+  {
+    int a=5, b=5, c=5, d=5;
+    System.out.println("The value of a is: "+a+++", the value of b is: "+b++);
+    System.out.println("The value of a is: "+a+++", the value of b is: "+b++);
+    System.out.println("The value of a is: "+a+++", the value of b is: "+b++);
+    System.out.println("The value of a is: "+a+++", the value of b is: "+b++);
+    System.out.println("The value of a is: "+a+++", the value of b is: "+b++);
+    System.out.println("The value of a is: "+a+++", the value of b is: "+b++);
+    System.out.println("--------------------------------------------------\n");
+    System.out.print("\n The value of c is : ");
+    System.out.print(++c+",");
+    System.out.print("The value of d is : ");
+    System.out.print(++d+"\n");
+    System.out.print("\n The value of c is : ");
+    System.out.print(++c+",");
+    System.out.print("The value of d is : ");
+    System.out.print(++d+"\n");
+    System.out.print("\n The value of c is : ");
+    System.out.print(++c+",");
+    System.out.print("The value of d is : ");
+    System.out.print(++d+"\n");
+    System.out.print("\n The value of c is : ");
+    System.out.print(++c+",");
+    System.out.print("The value of d is : ");
+    System.out.print(++d+"\n");
+    System.out.print("\n The value of c is : ");
+    System.out.print(++c+",");
+    System.out.print("The value of d is : ");
+    System.out.print(++d+"\n");
+    System.out.print("\n The value of c is : ");
+    System.out.print(++c+",");
+    System.out.print("The value of d is : ");
+    System.out.print(++d+"\n");
+  }
+}
+
+
+
+
+
+
